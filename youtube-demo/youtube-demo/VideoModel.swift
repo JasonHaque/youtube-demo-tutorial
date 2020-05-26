7edd1eb51593a30abccb89729fa6152bd0bcb70a
JasonHaque <zahinjason220434@gmail.com>
2020-05-27 03:54:46 +0600

    added video struct

---
+++ youtube-demo/youtube-demo/VideoModel.swift
@@ -0,0 +1,16 @@
+//
+//  VideoModel.swift
+//  youtube-demo
+//
+//  Created by Sanviraj Zahin Haque on 27/5/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
+
+class VideoModel{
+    
+    func getVideos(){
+        
+    }
+}
