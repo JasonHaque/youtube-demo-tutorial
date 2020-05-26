9745edb39b6313771e00ef3267f7c298ed67879b
JasonHaque <zahinjason220434@gmail.com>
2020-05-27 03:50:25 +0600

    constants added

---
+++ youtube-demo/youtube-demo/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  youtube-demo
+//
+//  Created by Sanviraj Zahin Haque on 27/5/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
+
+struct Constants{
+    
+    static var API_KEY = "AIzaSyBXEe-Qvds_GVWHnz2zQBfoNhSwT5AG_mo"
+    static var PLAYLIST_ID = "UUXuqSBlHAE6Xw-yeJA0Tunw"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
