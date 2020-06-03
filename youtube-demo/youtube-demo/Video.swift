6ab70833b5a8219b063b0d24ce244525eeb36193
JasonHaque <zahinjason220434@gmail.com>
2020-06-03 09:22:18 +0600

    decoding parameters added

--- youtube-demo/youtube-demo/Video.swift
+++ youtube-demo/youtube-demo/Video.swift
@@ -15,7 +15,7 @@ struct Video:Decodable{
     var title = ""
     var description = ""
     var thumbnail = ""
-    var published = ""
+    var published = Date()
     
     enum CodingKeys : String,CodingKey {
         
@@ -31,6 +31,37 @@ struct Video:Decodable{
         case videoId
     }
     init(from decoder:Decoder)throws{
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let snippetContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .snippet)
+        
+        //parse  title
+        
+        self.title = try snippetContainer.decode(String.self, forKey: .title)
+        
+        //parse description
+        
+        self.description = try snippetContainer.decode(String.self, forKey: .description)
+        
+        //parse published date
+        
+        self.published = try snippetContainer.decode(Date.self, forKey: .published)
+        
+        //parse the thumbnails
+        
+        let thumbnailContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .thumbnails)
+        
+        let highContainer = try thumbnailContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .high)
+        
+        self.thumbnail = try highContainer.decode(String.self, forKey: .thumbnail)
+        
+        //parse videoId
+        
+        let resourceContainer = try snippetContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .resourceId)
+        
+        self.videoId = try resourceContainer.decode(String.self, forKey: .videoId)
+        
         
         
     }
