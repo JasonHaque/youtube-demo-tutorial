db08de479a2bcfc74265aff20e9d4769b344282f
JasonHaque <zahinjason220434@gmail.com>
2020-06-11 11:26:37 +0600

    table view added

--- youtube-demo/youtube-demo/Constants.swift
+++ youtube-demo/youtube-demo/Constants.swift
@@ -13,4 +13,5 @@ struct Constants{
     static var API_KEY = "AIzaSyBXEe-Qvds_GVWHnz2zQBfoNhSwT5AG_mo"
     static var PLAYLIST_ID = "UUXuqSBlHAE6Xw-yeJA0Tunw"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    static var VIDEO_CELL_ID = "VideoCell"
 }
--- youtube-demo/youtube-demo/ViewController.swift
+++ youtube-demo/youtube-demo/ViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
     
     let  model = VideoModel()
+    var videos = [Video]()
 
+    @IBOutlet weak var VideoTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         model.getVideos()
         
+        VideoTableView.dataSource = self
+        VideoTableView.delegate = self
+        
     }
+    
+    // MARK:- Table View Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEO_CELL_ID, for: indexPath)
+        
+        return cell
+    }
+    
+    
 
 
 }
