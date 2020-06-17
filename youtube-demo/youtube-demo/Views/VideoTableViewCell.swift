6cbb7d7d733b4aeaa2c76b440b31d1a8a2304238
JasonHaque <zahinjason220434@gmail.com>
2020-06-17 10:52:19 +0600

    added ui elements

---
+++ youtube-demo/youtube-demo/Views/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  youtube-demo
+//
+//  Created by Sanviraj Zahin Haque on 17/6/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
