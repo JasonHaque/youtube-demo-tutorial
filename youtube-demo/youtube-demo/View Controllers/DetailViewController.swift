eb36ba65f40a4e6fc6f9bc8f0455e4c87b362d7f
JasonHaque <zahinjason220434@gmail.com>
2020-06-24 00:57:12 +0600

    new view controllers added

---
+++ youtube-demo/youtube-demo/View Controllers/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  youtube-demo
+//
+//  Created by Sanviraj Zahin Haque on 24/6/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
